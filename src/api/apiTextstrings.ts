612c0951f39b33e12dde25f25250329d8a49b913
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-07-21 21:41:01 +0300

    delete unnecessary header

--- src/api/apiTextstrings.ts
+++ src/api/apiTextstrings.ts
@@ -1,12 +1,5 @@
 export const apiTextstrings = {
-    async getTextById(id: number): Promise<string> {
-        let headers = new Headers();
-
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('TMG-Api-Key', '0J/RgNC40LLQtdGC0LjQutC4IQ==');
-        headers.append('Origin','http://localhost:3000');
-        
+    async getTextById(id: number): Promise<string> {        
         const responce = await fetch(`https://tmgwebtest.azurewebsites.net/api/textstrings/${id}`, {
             headers: {
                 'Origin': ' http://localhost:3000',
